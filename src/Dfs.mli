c217dbcc0a2e87f2bf9be7a1e9363e4bc60a9034
Paul ALNET <alnet@insa-toulouse.fr>
2025-12-07 21:15:12 +0100

    refactor: recursive dfs

--- src/Dfs.mli
+++ src/Dfs.mli
@@ -1,4 +1,6 @@
 open Graph
 
-
-val dfs : 'a graph -> id -> id -> 'a arc list option
+(* `dfs gr visited src tgt` performs a depth-first path search through graph from node src to node tgt,
+   having already visited (or banned) the nodes in visited.
+   Returns the traversed arcs, in order. *)
+val dfs : 'a graph -> id list -> id -> id -> 'a arc list option
