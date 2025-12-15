bc911e15a789b9b2be1850481232ce57da751f2b
Paul ALNET <alnet@insa-toulouse.fr>
2025-12-15 15:55:07 +0100

    refactor: use the label replacement feature of new_arc
    Unfortunately missed that when writing tools for the first time.

--- src/tools.ml
+++ src/tools.ml
@@ -2,16 +2,13 @@ open Graph
 
 let clone_nodes gr = n_fold gr new_node empty_graph
 
-let gmap_entire_arc gr f =
-  e_fold gr (fun acu edge -> new_arc acu (f edge)) (clone_nodes gr)
-
-let gmap gr f = gmap_entire_arc gr (fun edge -> { edge with lbl = f edge.lbl })
+let gmap gr f =
+  e_fold gr
+    (fun acu edge -> new_arc acu { edge with lbl = f edge.lbl })
+    (clone_nodes gr)
 
 let add_arc gr id1 id2 n =
-  let add_if_target_edge edge =
-    if edge.src = id1 && edge.tgt = id2 then { edge with lbl = edge.lbl + n }
-    else edge
+  let initial_value =
+    match find_arc gr id1 id2 with None -> 0 | Some edge -> edge.lbl
   in
-  match find_arc gr id1 id2 with
-  | None -> new_arc gr { src = id1; tgt = id2; lbl = n }
-  | Some _edge -> gmap_entire_arc gr add_if_target_edge
+  new_arc gr { src = id1; tgt = id2; lbl = initial_value + n }
