6e9ceacbffc11034d15704774c479ab31e4894e2
Paul ALNET <alnet@insa-toulouse.fr>
2026-01-06 09:43:54 +0100

    feat: busacker_gowen min cost algo

---
+++ src/max_flow_min_cost.mli
@@ -0,0 +1,14 @@
+open Graph
+
+type flow_cost_arc_lbl = { capacity : int; flow : int; cost : int }
+
+(* `busacker_gowen gr src tgt` applies the Busacker-Gowen algorithm (using
+   Bellman-Ford as a path-finding algorithm) to the capacity graph gr from node
+   src to node tgt.
+  Does NOT support return arcs (max 1 arc between each pair of nodes).
+  TODO confirm line above.
+  Costs must be positive.
+  TODO confirm line above.
+  *)
+val busacker_gowen :
+  flow_cost_arc_lbl graph -> id -> id -> flow_cost_arc_lbl graph
